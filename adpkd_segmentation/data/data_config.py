287385b2b7ef03e2036359150f71d1c82048e10f
aksg87 <akshay.k.goel@gmail.com>
2020-08-18 10:25:53 -0400

    use pathlib for data path specifications

--- adpkd_segmentation/data/data_config.py
+++ adpkd_segmentation/data/data_config.py
@@ -1,11 +1,11 @@
-import os
+from pathlib import Path
 
 # %%
-dataroot = "/mnt/Samsung SSD/vast_ai_mirror/"
+dataroot = Path("/mnt/Samsung SSD/vast_ai_mirror/")
 
 labeled_dirs = [
-    os.path.join(dataroot, "training-data-01-60MR"),
-    os.path.join(dataroot, "training_data-61-110MR_AX_SSFSE_ABD_PEL_50"),
+    dataroot / "training-data-01-60MR",
+    dataroot / "training_data-61-110MR_AX_SSFSE_ABD_PEL_50",
 ]
 
-unlabeled_dirs = [os.path.join(dataroot, "unlabelled_data")]
+unlabeled_dirs = [dataroot / "unlabelled_data"]
